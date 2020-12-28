72d7cccd9e38b07c49efde8ba21670c2209e5e8a
lucho1503 <lmmc.1503@gmail.com>
2020-12-28 01:00:48 -0500

    shebang

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,7 @@ import sys
 import os.path as path
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         exit(1)
     elif not path.exists(sys.argv[1]):
