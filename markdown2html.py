991cacad50ea8940b3f4ca3f8989e332b2f6e64d
lucho1503 <lmmc.1503@gmail.com>
2020-12-28 00:40:23 -0500

    shebang

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""
+convert a markdown to HTML files
+"""
 import markdown
 import sys
 import os.path as path
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print('Usage: ./markdown2html.py README.md README.html')
+        print('Usage: ./markdown2html.py README.md README.html',
+file=sys.stderr)
         exit(1)
     if not path.exists(sys.argv[1]):
-        print('Missing <filename>')
+        print('Missing <filename>', file=sys.stderr)
         exit(1)
     markdown.markdownFromFile(
         input=sys.argv[1],
