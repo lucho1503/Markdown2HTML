2832156a0595db760338d0993488288917894082
lucho1503 <lmmc.1503@gmail.com>
2020-12-28 01:08:52 -0500

    fixed

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,7 @@ import sys
 import os.path as path
 
 if __name__ == "__main__":
+    st = ""
     if len(sys.argv) < 3:
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         exit(1)
@@ -14,9 +15,18 @@ if __name__ == "__main__":
         sys.stderr.write('Missing ' + sys.argv[1] + '\n')
         exit(1)
     else:
-        markdown.markdownFromFile(
-            input=sys.argv[1],
-            output=sys.argv[2],
-            encoding='utf-8',
-        )
-        exit(0)
+        #markdown.markdownFromFile(
+        #    input=sys.argv[1],
+        #    output=sys.argv[2],
+        #    encoding='utf-8',
+        #)
+        #exit(0)
+        pass
+
+    with open(sys.argv[1], 'r') as r:
+        for line in r.readlines():
+            st += str(parse(line) + '\n')
+
+    with open(sys.argv[2], 'w') as w:
+        for line in st:
+            writer.write(line)
